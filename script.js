4bb0ea5f5ca8b7a08151d80b9b68479412746020
tildaengberg <tildaengberg@hotmail.com>
2022-05-02 22:49:37 +0200

    index ej centrerat

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const accordionBtns = document.querySelectorAll(".item-header");
 
 accordionBtns.forEach((accordion) => {
-  accordion.onclick = function () {
-    this.classList.toggle("active");
+    accordion.onclick = function () {
+        this.classList.toggle("active");
 
-    let content = this.nextElementSibling;
-    console.log(content);
+        let content = this.nextElementSibling;
+        console.log(content);
 
-    if (content.style.maxHeight) {
-      //this is if the accordion is open
-      content.style.maxHeight = null;
-    } else {
-      //if the accordion is currently closed
-      content.style.maxHeight = content.scrollHeight + "px";
-      console.log(content.style.maxHeight);
-    }
-  };
+        if (content.style.maxHeight) {
+            //this is if the accordion is open
+            content.style.maxHeight = null;
+        } else {
+            //if the accordion is currently closed
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+    };
 });
