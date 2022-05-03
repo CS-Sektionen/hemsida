28e9c61b64117328c6292eea6fe3df5958121409
tildaengberg <tildaengberg@hotmail.com>
2022-05-03 11:16:58 +0200

    navbar å städ

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ accordionBtns.forEach((accordion) => {
         }
     };
 });
+
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
